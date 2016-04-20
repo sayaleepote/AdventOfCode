64a9e08327ffd7127b7c534ff74907748f2d5e37
Sayalee Pote <sayaleepote44@gmail.com>
2016-04-20 10:54:51 +0530

    "Commit from Git GUI"

--- Program17/Program.cs
+++ Program17/Program.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program17
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Program17 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
             int[] containers = { 50, 44, 11, 49, 42, 46, 18, 32, 26, 40, 21, 7, 18, 43, 10, 47, 36, 24, 22, 40 };
             int sum = 0;
-            for (int i = 0; i < containers.Length; i++)
-            {
+            for (int i = 0; i < containers.Length; i++) {
                 while (sum <= 150) { 
 
                 }
